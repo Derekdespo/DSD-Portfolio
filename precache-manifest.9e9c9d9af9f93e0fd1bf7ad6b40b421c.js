0599f108a6b41c47bb25fbf0a6f022153fa1de5d
Derek Desposito <derekdespo@icloud.com>
2020-08-14 22:41:48 -0400

    Updates

---
+++ precache-manifest.9e9c9d9af9f93e0fd1bf7ad6b40b421c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ad0f46737beac45225a8d7552569bc",
+    "url": "/DSD-Portfolio/index.html"
+  },
+  {
+    "revision": "c8f16d3b9c0538f83eac",
+    "url": "/DSD-Portfolio/static/css/main.f7c95e34.chunk.css"
+  },
+  {
+    "revision": "60ebd90d0686a885a938",
+    "url": "/DSD-Portfolio/static/js/2.1f58ddb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DSD-Portfolio/static/js/2.1f58ddb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f16d3b9c0538f83eac",
+    "url": "/DSD-Portfolio/static/js/main.5db8151d.chunk.js"
+  },
+  {
+    "revision": "21b893ca24a57af4e104",
+    "url": "/DSD-Portfolio/static/js/runtime-main.81b1c822.js"
+  },
+  {
+    "revision": "95d9c5f1c64329e00fd4c1502053faeb",
+    "url": "/DSD-Portfolio/static/media/seoulwall.95d9c5f1.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DSD-Portfolio/precache-manifest.9e9c9d9af9f93e0fd1bf7ad6b40b421c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DSD-Portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
